36be265ecbe0b616eb8a97d8457166656601fe9a
Joseph Sijo <sijokannattumadom@gmail.com>
2026-01-14 13:35:29 +0530

    chore: sync latest changes to cloud

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from "./App.tsx";
 import { FinanceProvider } from "./context/FinanceContext.tsx";
 import { ThemeProvider } from "./components/ui/theme-provider.tsx";
 import "./index.css";
+import { registerSW } from 'virtual:pwa-register';
+
+// Register service worker for PWA
+registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <FinanceProvider>
